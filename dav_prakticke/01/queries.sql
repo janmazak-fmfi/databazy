de0f68d5f90d758f1513cc9589da8dbbfdcf0eea
Jan Mazak <jan.mazak@fmph.uniba.sk>
2025-09-23 11:56:02 +0200

    dav p1, c01

--- dav_prakticke/01/queries.sql
+++ dav_prakticke/01/queries.sql
@@ -8,8 +8,8 @@ FROM employee;
 
 /* 2. List names and jobs of employees with salary at least 2000. */
 
-/* 3. (1 point) Print names and salaries of all managers and analysts (in a single SELECT query). */
+/* 3. Print names and salaries of all managers and analysts (in a single SELECT query). */
 
-/* 4. Print the number of the department in which the president works. (Your query must not use a specific empno.) */
+/* 4. Print the id of the department in which the president works. (Your query must not use a specific emp_id.) */
 
-/* 5. (1 point) Print all employees that have names ending with the letter D and salary between 1000 and 2000. */
+/* 5. (1 point) Print all employees that have names beginning with the letter M and salary between 1000 and 2000. */
