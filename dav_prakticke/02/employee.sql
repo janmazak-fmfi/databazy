a639bdad937683d99160e722cc8395f4a7e5fe55
Jan Mazak <jan.mazak@fmph.uniba.sk>
2025-09-29 11:41:17 +0200

    dav p2, c2

---
+++ dav_prakticke/02/employee.sql
@@ -0,0 +1,30 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE department(
+   dept_id INTEGER PRIMARY KEY,
+   name VARCHAR(50) NOT NULL,
+   location VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE employee(
+   emp_id INTEGER PRIMARY KEY,
+   name VARCHAR(50) NOT NULL,
+   job VARCHAR(50) NOT NULL,
+   salary DECIMAL(10, 2),
+   superior INTEGER,
+   dept_id INTEGER NOT NULL
+);
+
+INSERT INTO department VALUES (10, 'Accounting', 'New York');
+INSERT INTO department VALUES (20, 'Research', 'Dallas');
+INSERT INTO department VALUES (30, 'Sales', 'Chicago');
+
+INSERT INTO employee VALUES (11, 'King', 'president', 5000, NULL, 10);
+INSERT INTO employee VALUES (12, 'AccountingManager', 'manager', 1500, 11, 10);
+INSERT INTO employee VALUES (13, 'AccountingClerk', 'clerk', 1300, 12, 10);
+INSERT INTO employee VALUES (14, 'SalesManager', 'manager', 2975, 11, 30);
+INSERT INTO employee VALUES (15, 'MoneyMagnet', 'salesman', 2650, 14, 30);
+INSERT INTO employee VALUES (16, 'DealMaster', 'salesman', 1900, 14, 30);
+INSERT INTO employee VALUES (17, 'MrFixIt', 'clerk', 950, 14, 30);
+INSERT INTO employee VALUES (18, 'InsightNinja', 'analyst', 3000, 14, 30);
---
+++ dav_prakticke/02/queries.sql
@@ -0,0 +1,32 @@
+/* ------------------------------------------------------------------------ */
+/* Write and test the following queries ----------------------------------- */
+/* ------------------------------------------------------------------------ */
+
+/*
+    DO NOT use aggregate functions like MIN() in the following queries.
+    Also, avoid ORDER BY and LIMIT to determine minimum.
+    For now, we are practicing using WHERE NOT EXISTS when calculating minimum.
+*/
+
+/* 1. Print all jobs (each only once). */
+
+/* 2. List names of all employees together with the location where they work. */
+
+/* 3. Print the name of the department in which the president works. (Your query must not use a specific emp_id.) */
+
+/* 4. Print jobs of employees who work in Chicago. (Avoid repetition. Your query must not use a specific dept_id.) */
+
+/* 5. (1 point) For each employee, list all his/her subordinates (those that have him/her as the superior).
+Use an outer join so that your list contains every employee, even those without subordinates. */
+
+/* 6. Which departments are empty (have no employees)? */
+
+/* 7. Find the lowest salary in New York. (Remember: no LIMIT, no MIN. Use NOT EXISTS.) */
+
+/* 8. (1 point) Which departments employ only salesmen? */
+
+/* 9. Which employees manage only clerks? (Interpretation: at least one clerk and only clerks.) */
+
+/* 10. (1 point) Which departments have an employee for every job in the database? (Your query must not rely on specific jobs in the given dataset.) */
+
+/* 11. Print names, department names and salaries of all employees whose salary is greater than the lowest salary in department 20. */
