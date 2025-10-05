0d3048e935c2c29ff439356727882d4dcd38d3ca
Jan Mazak <jan.mazak@fmph.uniba.sk>
2025-10-06 00:57:44 +0200

    dav p3, c3

---
+++ dav_prakticke/03/queries.sql
@@ -0,0 +1,31 @@
+/* ------------------------------------------------------------------------ */
+/* Write and test the following queries ----------------------------------- */
+/* ------------------------------------------------------------------------ */
+
+/* You can use aggregate functions, e.g. MIN, SUM, COUNT. */
+/* You might want to add records in the tables for testing correctness of your answers. Feel free to modify 'employee.sql'. */
+
+/* 1. How many employees are there? */
+
+/* 2. How many employees work on at least one project? */
+
+/* 3. (1 point) Which departments have at least two employees such that each of them is assigned to at least two projects? */
+
+/* 4. What is the total yearly payroll (sum of 12 * monthly salaries)? */
+
+/* 5. Using regular expressions, list all projects with name containing the letter V (upper or lower case).
+Docs: https://www.postgresql.org/docs/current/functions-matching.html */
+
+/* 6. List all projects with name starting with N, each of them with all employees assigned to it. */
+
+/* 7. Which departments have at least one employee for each of the projects? */
+
+/* 8. How many employees participate in all of the projects? */
+
+/* 9. Find all departments such that each of their employees is assigned to a project. */
+
+/* 10. For each employee, count the number of projects he works on. */
+
+/* 11. (1 point) Find employees working on all projects except exactly one. */
+
+/* 12. How many times more does the president earn than the lowest-paid employee? */
